ea170f7ac1ba50b8e3c2d45acc4f133551d50a8d
presisco <internight@sina.com>
2015-12-28 12:54:59 +0800

    add init support for bluetooth control

---
+++ app/src/main/java/newbeeideas/motionlight/DefinedKeyword.java
@@ -0,0 +1,34 @@
+package newbeeideas.motionlight;
+
+import android.util.Log;
+
+/**
+ * Created by presisco on 2015/12/28.
+ */
+public class DefinedKeyword {
+    public static String KEYWORD_RED="我很烦";
+    public static String KEYWORD_BLUE="我很抑郁";
+    public static String KEYWORD_GREEN="我很高兴";
+    public static String KEYWORD_CLOSE="关闭";
+
+    private static String BT_CMD_RED="1";
+    private static String BT_CMD_BLUE="2";
+    private static String BT_CMD_GREEN="3";
+    private static String BT_CMD_CLOSE="4";
+
+    public static String getBTCmd(String voice_input){
+        String cmd="4";
+        if(voice_input.equals(KEYWORD_RED)){
+            cmd=BT_CMD_RED;
+        }else if(voice_input.equals(KEYWORD_GREEN)){
+            cmd=BT_CMD_GREEN;
+        }else if(voice_input.equals(KEYWORD_BLUE)){
+            cmd=BT_CMD_BLUE;
+        }else if(voice_input.equals(KEYWORD_CLOSE)){
+            cmd=BT_CMD_CLOSE;
+        }else{
+            Log.d("DefinedKeyword","voice_input doesnt match any of the preset");
+        }
+        return cmd;
+    }
+}
--- app/src/main/java/newbeeideas/motionlight/MainActivity.java
+++ app/src/main/java/newbeeideas/motionlight/MainActivity.java
@@ -1,6 +1,8 @@
 package newbeeideas.motionlight;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -17,22 +19,37 @@ import android.widget.Toast;
 import com.iflytek.cloud.InitListener;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
-import com.iflytek.cloud.SpeechRecognizer;
-import com.iflytek.cloud.RecognizerListener;
 import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
-import com.iflytek.speech.SpeechRecognizerAidl;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
 
 public class MainActivity extends Activity {
 
     private InitListener mInitListener;
+    private BluetoothSPP mBluetoothHelper;
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(mBluetoothHelper.isBluetoothEnabled()){
+            mBluetoothHelper.setupService();
+            mBluetoothHelper.startService(BluetoothState.DEVICE_OTHER);
+        }else{
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBluetoothHelper=new BluetoothSPP(this);
 
         mInitListener=new InitListener() {
             @Override
@@ -40,39 +57,48 @@ public class MainActivity extends Activity {
 
             }
         };
-        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=567e7f91,"+SpeechConstant.FORCE_LOGIN+"=true");
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                RecognizerDialog iatDialog=new RecognizerDialog(MainActivity.this,mInitListener);
-                iatDialog.setListener(new RecognizerDialogListener() {
-                    @Override
-                    public void onResult(RecognizerResult recognizerResult, boolean b) {
-                        onRecognizeComplete(recognizerResult.getResultString());
-                    }
-
-                    @Override
-                    public void onError(SpeechError speechError) {
-
-                    }
-                });
-                iatDialog.show();
-//                SpeechRecognizer mlat=SpeechRecognizer.createRecognizer(MainActivity.this,null);
-//
-//                mlat.setParameter(SpeechConstant.DOMAIN,"iat");
-//                mlat.setParameter(SpeechConstant.LANGUAGE,"zh_cn");
-//                mlat.setParameter(SpeechConstant.ACCENT,"mandarin");
-//                mlat.startListening(mRecoListener);
-
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-            }
-        });
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=567e7f91," + SpeechConstant.FORCE_LOGIN + "=true");
     }
 
     public void onRecognizeComplete(String result){
         Toast.makeText(this,"Voice result:"+JsonParser.parseIatResult(result),Toast.LENGTH_LONG).show();
+        mBluetoothHelper.send(DefinedKeyword.getBTCmd(result),true);
+    }
+
+    public void onVoiceCmd(){
+        RecognizerDialog iatDialog=new RecognizerDialog(MainActivity.this,mInitListener);
+        iatDialog.setListener(new RecognizerDialogListener() {
+            @Override
+            public void onResult(RecognizerResult recognizerResult, boolean b) {
+                onRecognizeComplete(recognizerResult.getResultString());
+            }
+
+            @Override
+            public void onError(SpeechError speechError) {
+
+            }
+        });
+        iatDialog.show();
+    }
+
+    public void onSwitchCmd(){
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if(resultCode == Activity.RESULT_OK)
+                mBluetoothHelper.connect(data);
+        } else if(requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if(resultCode == Activity.RESULT_OK) {
+                mBluetoothHelper.setupService();
+                mBluetoothHelper.startService(BluetoothState.DEVICE_ANDROID);
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
